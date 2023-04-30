19122aea613301ddff41047a962b77d96671d83a
jgaviri <jgaviriat@unal.edu.co>
2023-04-29 20:10:50 -0500

    Modularización

--- Toma_fotos.py
+++ Toma_fotos.py
@@ -2,73 +2,77 @@ import cv2
 import os
 import numpy as np
 
-# Crea un objeto de captura de video
-cap = cv2.VideoCapture(0)
+def tomar_foto_video():
+    
+    '''Esta función se encarga de comenzar una grabación en vivo, buscar un objeto
+        amarillo(En este caso la placa), y tomar una foto cuando tenga un aréa mayor 
+        a 20000 y menor a 21000. La foto la guarda en la carpeta "fotos" '''
+    
+    
+    # Crea un objeto de captura de video
+    cap = cv2.VideoCapture(0)
 
-# Crea la carpeta si no existe
-if not os.path.exists('fotos'):
-    os.makedirs('fotos')
 
-while True:
-    # Lee un fotograma de la cámara
-    ret, frame = cap.read()
+    while True:
+        # Lee un fotograma de la cámara
+        ret, frame = cap.read()
 
-    # Convertir la imagen a espacio de color HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # Convertir la imagen a espacio de color HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Definir el rango de colores que deseas detectar en formato HSV
-    # Aquí se define el rango de amarillo
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([30, 255, 255])
+        # Definir el rango de colores que deseas detectar en formato HSV
+        # Aquí se define el rango de amarillo
+        lower_yellow = np.array([20, 100, 100])
+        upper_yellow = np.array([30, 255, 255])
 
-    # Aplicar la máscara para detectar solo los píxeles amarillos
-    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        # Aplicar la máscara para detectar solo los píxeles amarillos
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-    # Encontrar los contornos en la imagen de la máscara
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Encontrar los contornos en la imagen de la máscara
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Si se encontraron contornos
-    if len(contours) > 0:
-        # Encontrar el contorno más grande (el contorno de la parte amarilla)
-        max_contour = max(contours, key=cv2.contourArea)
+        # Si se encontraron contornos
+        if len(contours) > 0:
+            # Encontrar el contorno más grande (el contorno de la parte amarilla)
+            max_contour = max(contours, key=cv2.contourArea)
 
-        # Encontrar el rectángulo que rodea el contorno
-        x, y, w, h = cv2.boundingRect(max_contour)
-        
-        #Calcular aréa
-        area = w * h
-        print(area)
-        
-        #
-        if area >20000 and area<21000:
+            # Encontrar el rectángulo que rodea el contorno
+            x, y, w, h = cv2.boundingRect(max_contour)
+            
+            #Calcular aréa
+            area = w * h
+            print(area)
+            
+            #
+            if area >20000 and area<21000:
+                # Guarda la foto en la carpeta "fotos"
+                cv2.imwrite('fotos/imagen.png', frame)
+            
+            
+            # Dibujar un rectángulo alrededor del contorno
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Muestra el fotograma en una ventana
+        cv2.imshow('Camara', frame)
+
+        # Espera a que se presione una tecla
+        key = cv2.waitKey(1)
+
+        # Si se presiona la tecla "q", toma una foto
+        #if key == ord('q'):
             # Guarda la foto en la carpeta "fotos"
-            cv2.imwrite('fotos/imagen.png', frame)
-        
-        
-        # Dibujar un rectángulo alrededor del contorno
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #    cv2.imwrite('fotos/imagen.png', frame)
 
-    # Muestra el fotograma en una ventana
-    cv2.imshow('Camara', frame)
+        # Si se presiona la tecla "ESC", sale del bucle
+        if key == 27:
+            break
 
-    # Espera a que se presione una tecla
-    key = cv2.waitKey(1)
-
-    # Si se presiona la tecla "q", toma una foto
-    #if key == ord('q'):
-        # Guarda la foto en la carpeta "fotos"
-    #    cv2.imwrite('fotos/imagen.png', frame)
-
-    # Si se presiona la tecla "ESC", sale del bucle
-    if key == 27:
-        break
-
-# Libera la cámara y cierra la ventana
-cap.release()
-cv2.destroyAllWindows()
+    # Libera la cámara y cierra la ventana
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-
+tomar_foto_video()
 
 
 
